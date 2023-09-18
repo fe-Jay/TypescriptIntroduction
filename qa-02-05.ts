d76be4d04b09193fb4f6753684c2b2a13e9ee9c2
jay <sinaelj@gmail.com>
2023-09-18 12:00:03 +0900

    📝 protected, static 키워드

---
+++ qa-02-05.ts
@@ -0,0 +1,59 @@
+// (숙제1) 다음 x, y, z 속성의 특징을 설명해보십시오.
+
+class XyzUser {
+    // class안에서 사용할 수 있는 static키워드.
+    // instance에서 변수나 함수 사용 불가
+    private static x = 10;
+
+    // class와 instance 모두에서 사용 가능. public 생략 가능(기본 값)
+    public static y = 20;
+
+    // extends로 확장한 class에서도 사용 가능.
+    protected z = 30;
+}
+
+// (숙제2) x 속성에 숫자를 더해주는 함수가 필요합니다.
+class Sum {
+    private static x = 10;
+    public static y = 20;
+    static addOne(param: number) {
+        Sum.x += param;
+    }
+    static printX() {
+        console.log(Sum.x);
+    }
+}
+
+Sum.addOne(3); // = Sum.x = 13
+Sum.addOne(4); // = Sum.x = 17
+Sum.printX(); // = 17
+
+// (숙제3) 이렇게 네모.draw()를 할 때마다
+// index.html에 가로 30px, 세로 30px, 배경색이 'red' 의 <div> 박스가 무작위로 배치되어야합니다.
+
+class Square {
+    x: number;
+    y: number;
+    color: string;
+    constructor(x: number, y: number, color: string) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+    }
+    draw() {
+        let random = Math.random();
+        let div = document.createElement("div");
+        div.style.width = `${this.x}px`;
+        div.style.height = `${this.y}px`;
+        div.style.backgroundColor = `${this.color}`;
+        div.style.position = "absolute";
+        div.style.top = `${random * 400}px`;
+        div.style.left = `${random * 400}px`;
+        document.body.appendChild(div);
+    }
+}
+
+let squareInstance = new Square(30, 30, "red");
+squareInstance.draw();
+squareInstance.draw();
+squareInstance.draw();
